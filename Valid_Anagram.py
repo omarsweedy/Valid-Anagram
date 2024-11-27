685d3d3a255d1c17e8e69a8d59a456e4f0bf8684
omar samir <omarsweedy@gmail.com>
2024-11-27 13:43:16 +0200

    Update Valid_Anagram.py

--- Valid_Anagram.py
+++ Valid_Anagram.py
@@ -14,31 +14,3 @@ class Solution:
     
 sol=Solution()
 print(sol.isAnagram("carrace","racecar"))
-
-
-
-# class Solution:
-#     def isAnagram(self, s: str, t: str) -> bool:
-#         if len(s) != len(t):
-#             return False
-
-#         countS, countT = {}, {}
-
-#         for i in range(len(s)):
-#             countS[s[i]] = 1 + countS.get(s[i], 0)
-#             countT[t[i]] = 1 + countT.get(t[i], 0)
-#         return countS == countT    
-             
-
-            
-
-
-
-
-
-
-
-
-
-sol=Solution()
-print(sol.isAnagram("racecar","carrace"))        
